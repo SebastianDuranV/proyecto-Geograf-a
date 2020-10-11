1d35fe1b20f6e64f1734d08ef54c1f2b64b6bb1b
SebastianDuranV <sebastia.duran@alumnos.uach.cl>
2020-10-11 20:38:14 +0000

    pagina en curso

--- App.py
+++ App.py
@@ -2,27 +2,16 @@ import os
 from flask import Flask, request, redirect, url_for, render_template, flash
 from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename
-import datetime
 import zipfile
-
-
-"""
-    Tareas a realizar en servidor:
-        -Rastreador de país de origen de ip 
-        -Buscador
-        -Autenticación para editar, crear y eliminar datos.
-        -Terminar de pulir el sistema de subida de archivos. (ZIP)
-"""
-
 
 app = Flask(__name__)
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = 'el perro se llama manjar'
 mysql = MySQL()
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'proyectoGeografia'
+app.config['MYSQL_HOST'] = 'sebastianDuran.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'sebastianDuran'
+app.config['MYSQL_PASSWORD'] = 'proyecto'
+app.config['MYSQL_DB'] = 'sebastianDuran$default'
 
 
 
@@ -32,10 +21,10 @@ mysql.init_app(app)
 
 def extractallZip(directory, namefile):
     fantasy_zip = zipfile.ZipFile(directory + '/' + namefile)
-    fantasy_zip.extractall(directory) 
+    fantasy_zip.extractall(directory)
     fantasy_zip.close()
     os.remove(directory + '/' + namefile)
-     
+
 
 
 ##   i   n   d   e   x -----
@@ -68,7 +57,7 @@ def listContend():
     return render_template('new/author/newAuthor.html')
 
 
-#####   A   U   T   O   R   ######  
+#####   A   U   T   O   R   ######
 
 @app.route('/new/author')
 def newAuthor():
@@ -99,21 +88,21 @@ def deleteAuthor(idAuthor):
     mysql.connection.commit()
     cur.close()
     return redirect(url_for("listAuthor"))
-    
-    
+
+
 
 @app.route('/update/add_author/<idAuthor>', methods = ['POST'])
 def updateAuthor(idAuthor):
     if request.method == 'POST':
         names = request.form['names']
         lastname = request.form['lastnames']
-        gradeAcademy = request.form['gradeAcademy'] 
+        gradeAcademy = request.form['gradeAcademy']
         descripttion = request.form['descripttion']
         contact = request.form['contact']
-        
+
         if request.files['photo']:
             photo = 1 # 1 : hay foto
-                      # 0 : no hay foto 
+                      # 0 : no hay foto
         else:
             photo = 0
 
@@ -132,7 +121,7 @@ def updateAuthor(idAuthor):
                         +'= %s' ,(names, lastname))
             data = cur.fetchall()
             cur.close()
-        
+
             directory = os.path.join('./static/uploaders/author', str(data[0][0]))
             os.mkdir(directory)
             app.config['UPLOAD_FOLDER'] = "./static/uploaders/author/"+ str(data[0][0])
@@ -150,13 +139,13 @@ def addAuthor():
     if request.method == 'POST':
         names = request.form['names']
         lastname = request.form['lastnames']
-        gradeAcademy = request.form['gradeAcademy'] 
+        gradeAcademy = request.form['gradeAcademy']
         descripttion = request.form['descripttion']
         contact = request.form['contact']
-        
+
         if request.files['photo']:
             photo = 1 # 1 : hay foto
-                      # 0 : no hay foto 
+                      # 0 : no hay foto
         else:
             photo = 0
 
@@ -186,13 +175,13 @@ def addAuthor():
 
 
 
-######   C   O   N   T   E   N   I   D   O   :::::: ######  
+######   C   O   N   T   E   N   I   D   O   :::::: ######
 
 @app.route('/<types>')
 def getListOfType(types):
     cur = mysql.connect.cursor()
     try:
-        cur.execute('SELECT ' + types + '.*, author.names, author.lastNames FROM ' + types 
+        cur.execute('SELECT ' + types + '.*, author.names, author.lastNames FROM ' + types
                     + ' LEFT JOIN author  ON ' + types + '.idAuthor = author.idAuthor;')
     except:
         return '<h2> NOT FOUND 404 <h2>'
@@ -236,11 +225,11 @@ def newRegisterType(nameType):
     #num_fields = len(cur.description)
     field_names = [i[0] for i in cur.description]
     #print(field_names)
-    return render_template('new/newNews.html', type = nameType 
+    return render_template('new/newNews.html', type = nameType
             ,description = field_names, authores=authores, category=category)
 
 
-@app.route('/new/add_<types>', methods = ['POST','GET'])
+@app.route('/new/add_<types>', methods = ['POST'])
 def addRegisterType(types):
     if request.method == 'POST':
         title = request.form['title']
@@ -250,21 +239,20 @@ def addRegisterType(types):
         else:
             article = None
         idAutor = request.form.get('autor')
-        idCategory = request.form.get('category')
+        try:
+            idCategory = request.form.get('category')
+        except:
+            print("no categoria")
         if request.form['Imagen-url']:
             imagen = request.form['Imagen-url']
         else:
             imagen = None
 
-        cur = mysql.connection.cursor()
-        if types != "map":
-            cur.execute('INSERT INTO '+ types + ' (title'+ types +',subtitle'+ types 
-                        +'article'+ types +', idAuthor , dateTime'+ types +', dateTime'+ types +'_update,idFilecategory, imagen'+ types +')'
-                        +' VALUES(%s,%s,%s,%s,NOW(),NOW(),%s,%s)',(title, subtitle,article, idAutor,idCategory,imagen))
-        else:
-            cur.execute('INSERT INTO map (titlemap,subtitlemap, filemap'
-                    + ', idAuthor , dateTimemap, dateTimemap_update,idFilecategory, imagenmap)'
-                    +' VALUES(%s,%s,%s,%s,NOW(),NOW(),%s,%s)',(title, subtitle, article,idAutor,idCategory,imagen))
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute('INSERT INTO '+ types +' VALUES(NULL,%s,%s,%s,NOW(),NOW(),%s,%s,%s)',(title, subtitle,article,imagen, idAutor,idCategory))
+        except:
+            return"no insertado "
 
         # Subir archivos
         if request.files['files']:
@@ -274,17 +262,20 @@ def addRegisterType(types):
             data = cur.fetchall()
             #print(data[0][0])
             cur.close()
-        
-            #directory = os.path.join('./static/uploaders/' + types, str(data[0][0]))
-            directory = './static/uploaders/' + types+'/' + str(data[0][0])
-            print(directory)
-            os.mkdir(directory)
-        
-            app.config['UPLOAD_FOLDER'] = "./static/uploaders/" + types +'/'+ str(data[0][0])
+
+            directory = os.path.join('./proyecto-Geograf-a/static/uploaders/' + types, str(data[0][0]))
+            #directory = './static/uploaders/' + types+'/' + str(data[0][0])
+
+            try:
+                os.makedirs(directory)
+            except:
+                return "no se pudo crear archivo" + directory
+
+            app.config['UPLOAD_FOLDER'] = "./proyecto-Geograf-a/static/uploaders/" + types +'/'+ str(data[0][0])
             f = request.files['files']
             filename = secure_filename(f.filename)
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            directory = "./static/uploaders/" + types +'/'+ str(data[0][0])
+            directory = "./proyecto-Geograf-a/static/uploaders/" + types +'/'+ str(data[0][0])
             extractallZip(directory, filename)
         mysql.connection.commit()
         flash(types +" added succesfully")
@@ -358,15 +349,18 @@ def update(types,idA):
         cur = mysql.connection.cursor()
         #  idCategory
         types = types[1::]
-        if types != "map":
-            cur.execute('update '+ types + ' set title'+ types +' = %s,subtitle'
-                        + types +' = %s, idAuthor = %s , dateTime'+ types +', dateTime'+ types +'_update = NOW(), idFilecategory = %s, imagen'+ types +' = %s'
-                        ,(title, subtitle, idAutor,idCategory,imagen))
-        else:
-            cur.execute('update map set titlemap = %s , subtitlemap = %s, filemap = %s'
-                    + ', idAuthor = %s , dateTimemap_update = NOW(),idFilecategory = %s, imagenmap = %s'
-                    ,(title, subtitle, article,idAutor,idCategory,imagen))
-        
+        try:
+            if types != "mapas":
+                cur.execute('update '+ types + ' set title'+ types +' = %s,subtitle'+ types + ' = %s,article'
+                            + types +' = %s, idAuthor = %s , dateTime'+ types +', dateTime'+ types +'_update = NOW(), idFilecategory = %s, imagen'+ types +' = %s'
+                            ,(title, subtitle,article, idAutor,idCategory,imagen))
+            else:
+                cur.execute('update mapas set titlemapas = %s , subtitlemapas = %s, filemapas = %s'
+                        + ', idAuthor = %s , dateTimemapas_update = NOW(),idFilecategory = %s, imagenmapas = %s'
+                        ,(title, subtitle, article,idAutor,idCategory,imagen))
+        except:
+            return "no se ha podido actualizar." + types
+
         if request.files['files']:
             # Cambiar esto::
             cur.execute('SELECT * FROM '+ types + ' WHERE title'+ types +' = %s AND  subtitle'
@@ -380,28 +374,65 @@ def update(types,idA):
             # directory = os.path.join('./static/uploaders/' + types, str(data[0][0]))
             directory = './static/uploaders/' + types+'/' + str(data[0][0])
             try:
-                os.mkdir(directory)
+                os.makedirs(directory)
             except:
                 print("Carpeta creada")
-            
+
             app.config['UPLOAD_FOLDER'] = "./static/uploaders/" + types +'/'+ str(data[0][0])
             f = request.files['files']
             filename = secure_filename(f.filename)
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             directory = "./static/uploaders/" + types +'/'+ str(data[0][0])
             extractallZip(directory, filename)
-        
+
         mysql.connection.commit()
         cur.close()
         return redirect(url_for("deleteEdit" , nameTypes = types))
 
 
-#### Buscador
-@app.route('/search')
-def search():
-    searchWords = request.form['searchWords']
-    
+#@app.route('/tablas', methods = ['POST','GET'])
+def crearTablas():
+    cur = mysql.connect.cursor()
+
+    #author
+    cur.execute('CREATE TABLE author (idAuthor INT  AUTO_INCREMENT , PRIMARY KEY (idAuthor), names VARCHAR(40) , lastnames VARCHAR(40), gradeAcademy VARCHAR(60), descripttion VARCHAR(200), contact VARCHAR (40)), photo BOOL DEFAULT 0;')
+
+    #categories
+    cur.execute('CREATE TABLE category (idFilecategory INT  AUTO_INCREMENT , PRIMARY KEY (idFilecategory), nameCategory VARCHAR(25));')
+
+    # noticias
+    cur.execute('CREATE TABLE noticias ( idnoticias INT  AUTO_INCREMENT , PRIMARY KEY (idnoticias), titlenoticias VARCHAR(180) , subtitlenoticias VARCHAR(200) , '
+                       +'articlenoticias MEDIUMTEXT , dateTimenoticias DATETIME , dateTimenoticias_update DATETIME ,imagennoticias VARCHAR(180)  '
+                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor)  , archiveTruenoticias BOOL DEFAULT 0 '
+                      +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory)  );')
+
+    # blog
+    cur.execute('CREATE TABLE blog ( idBlog INT  AUTO_INCREMENT , PRIMARY KEY (idBlog), titleBlog VARCHAR(180)  , subtitleBlog VARCHAR(200)  , '
+                        +'articleBlog MEDIUMTEXT  , dateTimeBlog DATETIME, dateTimeBlog_update DATETIME,  imagenBlog VARCHAR(180)  '
+                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTrueBlog BOOL DEFAULT 0'
+                       +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
+
+    #proyectos
+    cur.execute('CREATE TABLE proyectos ( idproyectos INT  AUTO_INCREMENT , PRIMARY KEY (idproyectos), titleproyectos VARCHAR(180) , subtitleproyectos VARCHAR(200) , '
+                        +'articleproyectos MEDIUMTEXT  , dateTimeproyectos DATETIME, dateTimeproyectos_update DATETIME,  imagenproyectos VARCHAR(180)  '
+                       +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTrueproyectos BOOL DEFAULT 0'
+                        +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
+
+    #mapass
+    cur.execute('CREATE TABLE mapas ( idmapas INT  AUTO_INCREMENT , PRIMARY KEY (idmapas), titlemapas VARCHAR(200) , subtitlemapas VARCHAR(200) , '
+                         +'filemapas VARCHAR(200)  , dateTimemapas DATETIME, dateTimemapas_update DATETIME,  imagenmapas VARCHAR(200)  '
+                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTruemapas BOOL DEFAULT 0'
+                        +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
+
+    #monitoreos
+    cur.execute('CREATE TABLE monitoreos ( idmonitoreos INT  AUTO_INCREMENT , PRIMARY KEY (idmonitoreos), titlemonitoreos VARCHAR(180) , subtitlemonitoreos VARCHAR(200) , '
+                         +'articlemonitoreos MEDIUMTEXT  , dateTimemonitoreos DATETIME, dateTimemonitoreos_update DATETIME,  imagenmonitoreos  VARCHAR(180)  '
+                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTruemonitoreos BOOL DEFAULT 0'
+                        +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
 
 
-if __name__== '__main__':
-    app.run(port = 2000, debug=True)
+
+
+    cur.close()
+    return "listo"
+
--- dataBases.py
+++ dataBases.py
@@ -5,70 +5,51 @@ app = Flask(__name__)
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = 'el perro se llama manjar'
 mysql = MySQL()
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'proyectoGeografia'
-
+app.config['MYSQL_HOST'] = 'sebastianDuran.mysql.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'sebastianDuran'
+app.config['MYSQL_PASSWORD'] = 'proyecto'
+app.config['MYSQL_DB'] = 'sebastianDuran$default'
 mysql.init_app(app)
-
-@app.route('/', methods = ['POST','GET'])
-def crearTablas():
-    cur = mysql.connect.cursor()
+cur = mysql.connect.cursor()
 
     #author
-    #cur.execute('CREATE TABLE author (idAuthor INT  AUTO_INCREMENT , PRIMARY KEY (idAuthor), names VARCHAR(40) , lastnames VARCHAR(40), gradeAcademy VARCHAR(60), descripttion VARCHAR(200), contact VARCHAR (40)), photo BOOL DEFAULT 0;')
+cur.execute('CREATE TABLE author (idAuthor INT  AUTO_INCREMENT , PRIMARY KEY (idAuthor), names VARCHAR(40) , lastnames VARCHAR(40), gradeAcademy VARCHAR(60), descripttion VARCHAR(200), contact VARCHAR (40)), photo BOOL DEFAULT 0;')
 
     #categories
-    #cur.execute('CREATE TABLE category (idFilecategory INT  AUTO_INCREMENT , PRIMARY KEY (idFilecategory), nameCategory VARCHAR(25));')
+cur.execute('CREATE TABLE category (idFilecategory INT  AUTO_INCREMENT , PRIMARY KEY (idFilecategory), nameCategory VARCHAR(25));')
 
-    # news
-    #cur.execute('CREATE TABLE news ( idNews INT  AUTO_INCREMENT , PRIMARY KEY (idNews), titleNews VARCHAR(180) , subtitleNews VARCHAR(200) , '
-    #                   +'articleNews MEDIUMTEXT , dateTimeNews DATETIME , dateTimeNews_update DATETIME ,imagenNews VARCHAR(180)  '
-    #                    +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor)  , archiveTrueNews BOOL DEFAULT 0 '
-    #                  +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory)  );')
+    # noticias
+cur.execute('CREATE TABLE noticias ( idnoticias INT  AUTO_INCREMENT , PRIMARY KEY (idnoticias), titlenoticias VARCHAR(180) , subtitlenoticias VARCHAR(200) , '
+                       +'articlenoticias MEDIUMTEXT , dateTimenoticias DATETIME , dateTimenoticias_update DATETIME ,imagennoticias VARCHAR(180)  '
+                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor)  , archiveTruenoticias BOOL DEFAULT 0 '
+                      +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory)  );')
 
     # blog
-    #cur.execute('CREATE TABLE blog ( idBlog INT  AUTO_INCREMENT , PRIMARY KEY (idBlog), titleBlog VARCHAR(180)  , subtitleBlog VARCHAR(200)  , '
-    #                    +'articleBlog MEDIUMTEXT  , dateTimeBlog DATETIME, dateTimeBlog_update DATETIME,  imagenBlog VARCHAR(180)  '
-    #                    +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTrueBlog BOOL DEFAULT 0'
-    #                   +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
+cur.execute('CREATE TABLE blog ( idBlog INT  AUTO_INCREMENT , PRIMARY KEY (idBlog), titleBlog VARCHAR(180)  , subtitleBlog VARCHAR(200)  , '
+                        +'articleBlog MEDIUMTEXT  , dateTimeBlog DATETIME, dateTimeBlog_update DATETIME,  imagenBlog VARCHAR(180)  '
+                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTrueBlog BOOL DEFAULT 0'
+                       +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
 
-    #Project
-    #cur.execute('CREATE TABLE projects ( idProject INT  AUTO_INCREMENT , PRIMARY KEY (idProject), titleProject VARCHAR(180) , subtitleProject VARCHAR(200) , '
-    #                    +'articleProject MEDIUMTEXT  , dateTimeProject DATETIME, dateTimeProject_update DATETIME,  imagenProject VARCHAR(180)  '
-    #                   +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTrueProject BOOL DEFAULT 0'
-    #                    +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
-
-    #maps
-    cur.execute('CREATE TABLE map ( idMap INT  AUTO_INCREMENT , PRIMARY KEY (idMap), titleMap VARCHAR(200) , subtitleMap VARCHAR(200) , '
-                         +'fileMap VARCHAR(200)  , dateTimeMap DATETIME, dateTimeMap_update DATETIME,  imagenMap VARCHAR(200)  '
-                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTrueMap BOOL DEFAULT 0'
+    #proyectos
+cur.execute('CREATE TABLE proyectos ( idproyectos INT  AUTO_INCREMENT , PRIMARY KEY (idproyectos), titleproyectos VARCHAR(180) , subtitleproyectos VARCHAR(200) , '
+                        +'articleproyectos MEDIUMTEXT  , dateTimeproyectos DATETIME, dateTimeproyectos_update DATETIME,  imagenproyectos VARCHAR(180)  '
+                       +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTrueproyectos BOOL DEFAULT 0'
                         +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
 
-    #monitoring
-    #cur.execute('CREATE TABLE monitoring ( idMonitoring INT  AUTO_INCREMENT , PRIMARY KEY (idMonitoring), titleMonitoring VARCHAR(180) , subtitleMonitoring VARCHAR(200) , '
-    #                     +'articleMonitoring MEDIUMTEXT  , dateTimeMonitoring DATETIME, dateTimeMonitoring_update DATETIME,  imagenMonitoring  VARCHAR(180)  '
-    #                    +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTrueMonitoring BOOL DEFAULT 0'
-    #                    +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
+    #mapass
+cur.execute('CREATE TABLE mapas ( idmapas INT  AUTO_INCREMENT , PRIMARY KEY (idmapas), titlemapas VARCHAR(200) , subtitlemapas VARCHAR(200) , '
+                         +'filemapas VARCHAR(200)  , dateTimemapas DATETIME, dateTimemapas_update DATETIME,  imagenmapas VARCHAR(200)  '
+                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTruemapas BOOL DEFAULT 0'
+                        +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
+
+    #monitoreos
+cur.execute('CREATE TABLE monitoreos ( idmonitoreos INT  AUTO_INCREMENT , PRIMARY KEY (idmonitoreos), titlemonitoreos VARCHAR(180) , subtitlemonitoreos VARCHAR(200) , '
+                         +'articlemonitoreos MEDIUMTEXT  , dateTimemonitoreos DATETIME, dateTimemonitoreos_update DATETIME,  imagenmonitoreos  VARCHAR(180)  '
+                        +',idAuthor int, FOREIGN KEY (idAuthor) REFERENCES author(idAuthor) , archiveTruemonitoreos BOOL DEFAULT 0'
+                        +',idFilecategory int, FOREIGN KEY (idFilecategory) REFERENCES category(idFilecategory));')
 
 
 
 
-    cur.close()
-    return 'listo'
-
-
-@app.route('/category', methods = ['POST','GET'])
-def category():
-    cur = mysql.connect.cursor()
-    namesCategory = ['Arduino', 'Tutoriales', 'Raspberri Pi','Python','Qgis']
-    for i in namesCategory:
-        
-        cur.execute('insert into category (nameCategory) VALUES(%s)',(i))
-    mysql.connection.commit()
-    cur.close()
-    return 'listo categoria'
-
-if __name__== '__main__':
-    app.run(port = 2001, debug=True)
+cur.close()
+print("listo")
